345a737857a41b58a619b1dd2022bcae480e4d1c
Alex <alxmorais8@msn.com>
2020-10-29 21:39:37 +0100

    Fix standard

---
+++ test/basic.js
@@ -0,0 +1,16 @@
+const webtorrentHealth = require('../index')
+
+const magnet = 'magnet:?xt=urn:btih:6a9759bffd5c0af65319979fb7832189f4f3c35d&dn=sintel.mp4&tr=udp%3A%2F%2Fexodus.desync.com%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&tr=wss%3A%2F%2Ftracker.webtorrent.io&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel-1024-surround.mp4'
+
+// Magnet link with no tracker
+// var magnet = 'magnet:?xt=urn:btih:6a9759bffd5c0af65319979fb7832189f4f3c35d&dn=sintel.mp4'
+
+// Torrent file with no tracker
+// var fs = require('fs')
+// var torrentId = fs.readFileSync(__dirname + '/sintel.torrent')
+
+webtorrentHealth(magnet, /* { trackers: ['wss://tracker.openwebtorrent.com'], timeout: 1500 }, */ function (err, result) {
+  if (err) return console.error(err)
+
+  return console.log(result)
+})
